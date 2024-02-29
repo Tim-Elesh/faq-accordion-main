2beceedb996bb01fb2820a0018d0615970be2789
Tim <146209162+Tim-Elesh@users.noreply.github.com>
2024-02-29 22:14:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function myFunction(toggle1){
   var imgElement = document.getElementById("svgImage");
   var currentSrc = imgElement.getAttribute("src");
   
-  if (currentSrc === "/faq-accordion-main/assets/images/icon-plus.svg") {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-minus.svg");
+  if (currentSrc === "icon-plus.svg") {
+    imgElement.setAttribute("src", "icon-minus.svg");
   } else {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-plus.svg");
+    imgElement.setAttribute("src", "icon-plus.svg");
   }
 
    let visible = document.getElementById("toggle1");
@@ -21,10 +21,10 @@ function myFunction2(toggle2){
   var imgElement = document.getElementById("svgImage2");
   var currentSrc = imgElement.getAttribute("src");
 
-  if (currentSrc === "/faq-accordion-main/assets/images/icon-plus.svg") {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-minus.svg");
+  if (currentSrc === "icon-plus.svg") {
+    imgElement.setAttribute("src", "icon-minus.svg");
   } else {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-plus.svg");
+    imgElement.setAttribute("src", "icon-plus.svg");
   }
 
   let visible = document.getElementById("toggle2");
@@ -36,10 +36,10 @@ function myFunction3(toggle3){
   var imgElement = document.getElementById("svgImage3");
   var currentSrc = imgElement.getAttribute("src");
 
-  if (currentSrc === "/faq-accordion-main/assets/images/icon-plus.svg") {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-minus.svg");
+  if (currentSrc === "icon-plus.svg") {
+    imgElement.setAttribute("src", "icon-minus.svg");
   } else {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-plus.svg");
+    imgElement.setAttribute("src", "icon-plus.svg");
   }
 
   let visible = document.getElementById("toggle3");
@@ -51,10 +51,10 @@ function myFunction4(toggle4){
   var imgElement = document.getElementById("svgImage4");
   var currentSrc = imgElement.getAttribute("src");
   
-  if (currentSrc === "/faq-accordion-main/assets/images/icon-plus.svg") {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-minus.svg");
+  if (currentSrc === "icon-plus.svg") {
+    imgElement.setAttribute("src", "icon-minus.svg");
   } else {
-    imgElement.setAttribute("src", "/faq-accordion-main/assets/images/icon-plus.svg");
+    imgElement.setAttribute("src", "icon-plus.svg");
   }
 
   let visible = document.getElementById("toggle4");
